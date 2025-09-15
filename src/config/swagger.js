f622fa62d612e43323bbd2d224797c51b4e78e1e
Fresnel Jean Claude <fresneljeanclaude@outlook.com>
2025-09-15 12:49:53 +0200

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,7 +1,17 @@
 // src/config/swagger.js
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../docs/swagger.json" assert { type: "json" };
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-export default (app) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Lecture du fichier swagger.json
+const swaggerPath = join(__dirname, "../docs/swagger.json");
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, "utf8"));
+
+// Fonction pour initialiser Swagger
+export default function setupSwagger(app) {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-};
+}
